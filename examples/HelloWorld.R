f8364af02545ee91216cb5e9055993ccf2a64fcc
Rob Brennan <rob@therobbrennan.com>
2023-02-09 21:34:34 -0800

    Simplify the Hello, world! example

--- examples/HelloWorld.R
+++ examples/HelloWorld.R
@@ -1,5 +1,2 @@
 # Simple R script to display the obligatory Hello, world! as well as the current time and timezone
-currentTime <- Sys.time()
-currentTimezone <- Sys.timezone()
-
-sprintf("Hello, world! This script is running at %s in the %s timezone.", currentTime, currentTimezone)
+sprintf("Hello, world! This script is running at %s in the %s timezone.", Sys.time(), Sys.timezone())
